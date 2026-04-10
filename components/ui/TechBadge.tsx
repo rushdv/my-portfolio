42be0b997d6e9cd05d1fd54ba6dbf98c39c08df7
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2026-04-10 15:06:13 +0600

    feat: initial commit

--- components/ui/TechBadge.tsx
+++
@@ -1,7 +0,0 @@
-export default function TechBadge({ name }: { name: string }) {
-  return (
-    <span className="inline-block px-2 py-1 text-xs rounded-md bg-white/10 text-white/80 border border-white/10">
-      {name}
-    </span>
-  )
-}
